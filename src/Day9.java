60237d7d614a0334ab61ac8e66683a64b54ec9a1
Jan-Niklas Röhlig <138626680+JNR306@users.noreply.github.com>
2024-12-10 13:29:30 +0100

    Improved performance of Day 9 significantly

--- src/Day9.java
+++ src/Day9.java
@@ -6,109 +6,101 @@ import java.util.stream.Stream;
 
 public class Day9 {
 
-    static Integer[] diskMapReadOnly;
-    static Integer[] diskMapWriteOnly;
+    static int[] diskMapReadOnly;
+    static int[] diskMapWriteOnly;
     static int[] sizesOfFileBlocks;
     static int[] firstAppearance;
 
-    public static void splitInput() {
-        int[] input = Stream.of(Input.INPUT_DAY_9)
+    public static void splitInput(boolean overwriteEverything) {
+        if (overwriteEverything) {
+            AtomicInteger idx = new AtomicInteger();
+            diskMapReadOnly = Stream.of(Input.INPUT_DAY_9)
+                    .flatMap(s -> s.chars().mapToObj(c -> String.valueOf((char) c))) //Stream<String>
+                    .mapToInt(Integer::parseInt)
+                    .mapToObj(i -> {
+                        int[] array = new int[i]; //generate an array of size i
+                        if (idx.get() % 2 == 0)
+                            Arrays.fill(array, idx.get() / 2); //fill it with idx/2 if even
+                        else
+                            Arrays.fill(array, -1); //fill it with null if odd (-1 should represent null in this entire program)
+                        idx.incrementAndGet();
+                        return Arrays.stream(array); //Stream<Integer>
+                    })
+                    .flatMapToInt(i -> i)
+                    .toArray();
+            firstAppearance = new int[diskMapReadOnly.length / 2];
+            int lastFoundFileId = -1;
+            for (int i = 0; i < diskMapReadOnly.length; i++) {
+                if (diskMapReadOnly[i] != -1 && diskMapReadOnly[i] != lastFoundFileId) {
+                    lastFoundFileId = diskMapReadOnly[i];
+                    firstAppearance[lastFoundFileId] = i;
+                }
+            }
+        }
+        int[] inputDigits = Stream.of(Input.INPUT_DAY_9)
                 .flatMap(s -> s.chars().mapToObj(c -> String.valueOf((char) c))) //Stream<String>
                 .mapToInt(Integer::parseInt)
                 .toArray();
-        sizesOfFileBlocks = new int[(input.length+1)/2];
-        for (int i = 0; i < input.length; i++) {
-            if (i % 2 == 0)
-                sizesOfFileBlocks[i/2] = input[i];
-        }
-        AtomicInteger idx = new AtomicInteger();
-        diskMapReadOnly = Stream.of(Input.INPUT_DAY_9)
-                .flatMap(s -> s.chars().mapToObj(c -> String.valueOf((char) c))) //Stream<String>
-                .mapToInt(Integer::parseInt)
-                .mapToObj(i -> {
-                    Integer[] array = new Integer[i]; //generate an array of size i
-                    if (idx.get() % 2 == 0)
-                        Arrays.fill(array, idx.get() / 2); //fill it with idx/2 if even
-                    else
-                        Arrays.fill(array, null); //fill it with null if odd
-                    idx.incrementAndGet();
-                    return Arrays.stream(array); //Stream<Integer>
-                })
-                .flatMap(i -> i)
-                .toArray(Integer[]::new);
         diskMapWriteOnly = diskMapReadOnly.clone();
-        firstAppearance = new int[diskMapReadOnly.length/2];
-        for (int j = 0; j < diskMapReadOnly.length/2; j++) {
-            for (int i = 0; i < diskMapReadOnly.length; i++) {
-                if (diskMapReadOnly[i] != null && diskMapReadOnly[i] == j) {
-                    firstAppearance[j] = i;
-                }
-            }
+        sizesOfFileBlocks = new int[(inputDigits.length + 1) / 2];
+        for (int i = 0; i < inputDigits.length; i++) {
+            if (i % 2 == 0)
+                sizesOfFileBlocks[i / 2] = inputDigits[i];
         }
     }
 
     public static void solvePartOne() {
-        int neededIterationsCount = countEmptySpaces();
-        for (int i = 0; i < neededIterationsCount; i++) {
-            Integer removedFileId = getAndRemoveLastFile();
-            addFile(removedFileId);
+        for (int i = 0; i < diskMapReadOnly.length; i++) {
+            if (diskMapReadOnly[i] == -1) {
+                fillWithFile(i);
+            }
+        }
+        for (int i = 0; i < diskMapReadOnly.length; i++) {
+            if (diskMapReadOnly[i] != -1 && sizesOfFileBlocks[diskMapReadOnly[i]] == 0) {
+                diskMapWriteOnly[i] = -1;
+            } else if (diskMapReadOnly[i] != -1) {
+                sizesOfFileBlocks[diskMapReadOnly[i]]--;
+            }
         }
         long checksum = 0L;
         for (int i = 0; i < diskMapWriteOnly.length; i++) {
-            if (diskMapWriteOnly[i] != null) {
+            if (diskMapWriteOnly[i] != -1) {
                 checksum += (long) i * diskMapWriteOnly[i];
             }
         }
         System.out.println("SOLUTION DAY 9 PART 1: " + checksum);
     }
 
-    private static void addFile(Integer fileId) {
-        for (int i = 0; i < diskMapWriteOnly.length; i++) {
-            if (diskMapWriteOnly[i] == null) {
-                diskMapWriteOnly[i] = fileId;
-                return;
+    private static void fillWithFile(int index) {
+        for (int i = sizesOfFileBlocks.length - 1; i >= 0; i--) {
+            if (sizesOfFileBlocks[i] > 0 && index <= getIndexOfFirstAppearance(i)) {
+                diskMapWriteOnly[index] = i;
+                sizesOfFileBlocks[i]--;
+                break;
             }
         }
-    }
-
-    private static Integer getAndRemoveLastFile() {
-        for (int i = diskMapWriteOnly.length - 1; i >= 0; i--) {
-            if (diskMapWriteOnly[i] != null) {
-                Integer fileId = diskMapWriteOnly[i];
-                diskMapWriteOnly[i] = null;
-                return fileId;
-            }
-        }
-        return null;
-    }
-
-    private static int countEmptySpaces() {
-        int count = 0;
-        for (Integer fileId : diskMapWriteOnly)
-            if (fileId == null) count++;
-        return count;
     }
 
     public static void solvePartTwo() {
         int startIndexOfEmptySpace = -1;
         int endIndexOfEmptySpace;
         for (int i = 0; i < diskMapReadOnly.length; i++) {
-            if (diskMapReadOnly[i] == null && startIndexOfEmptySpace == -1) {
+            if (diskMapReadOnly[i] == -1 && startIndexOfEmptySpace == -1) {
                 startIndexOfEmptySpace = i;
-            } else if (diskMapReadOnly[i] != null && startIndexOfEmptySpace != -1) {
+            } else if (diskMapReadOnly[i] != -1 && startIndexOfEmptySpace != -1) {
                 endIndexOfEmptySpace = i;
                 fillWithFileBlocks(startIndexOfEmptySpace, endIndexOfEmptySpace);
                 startIndexOfEmptySpace = -1;
             }
         }
         for (int i = 0; i < diskMapReadOnly.length; i++) {
-            if (diskMapReadOnly[i] != null && sizesOfFileBlocks[diskMapReadOnly[i]] == 0) {
-                diskMapWriteOnly[i] = null;
+            if (diskMapReadOnly[i] != -1 && sizesOfFileBlocks[diskMapReadOnly[i]] == 0) {
+                diskMapWriteOnly[i] = -1;
             }
         }
         long checksum = 0L;
         for (int i = 0; i < diskMapWriteOnly.length; i++) {
-            if (diskMapWriteOnly[i] != null) {
+            if (diskMapWriteOnly[i] != -1) {
                 checksum += (long) i * diskMapWriteOnly[i];
             }
         }
@@ -137,9 +129,9 @@ public class Day9 {
     }
 
     public static void main(String[] args) {
-        Day9.splitInput();
+        Day9.splitInput(true);
         Day9.solvePartOne();
-        Day9.splitInput();
+        Day9.splitInput(false);
         Day9.solvePartTwo();
     }
 }
