1018adaa5ef05ca02a5cd3948b296742e881f8f2
Jan-Niklas Röhlig <138626680+JNR306@users.noreply.github.com>
2024-12-13 21:17:02 +0100

    Consistently changed solutions to use the more efficient implementation

--- src/Day13.java
+++ src/Day13.java
@@ -17,33 +17,11 @@ public class Day13 {
     }
 
     public static void solvePartOne() {
-        long totalTokens = 0L;
-        for (long[] machine : machines) {
-            long a = Math.min(machine[4] / machine[0], 100);
-            while (a >= 0) {
-                long b = a * machine[0];
-                long c = machine[4] - b;
-                boolean mod = c % machine[2] == 0;
-                if (mod) {
-                    long d = c / machine[2];
-                    if (d <= 100) {
-                        if (a * machine[1] + d * machine[3] == machine[5]) {
-                            totalTokens += 3 * a + d;
-                            break;
-                        }
-                    }
-                }
-                a--;
-            }
-        }
+        long totalTokens = solve(100);
         System.out.println("SOLUTION DAY 13 PART 1: " + totalTokens);
     }
 
-    public static void solvePartTwo() {
-        for (int i = 0; i < machines.length; i++) {
-            machines[i][4] += 10000000000000L;
-            machines[i][5] += 10000000000000L;
-        }
+    private static long solve(Integer buttonPressLimit) {
         long totalTokens = 0L;
         for (long[] machine : machines) {
             //machine: [a,b,c,d,e,f]
@@ -59,13 +37,22 @@ public class Day13 {
             //if you solve this equation system, you will get the following two equations for x and y:
             long x = Math.round((f * c - d * e) / (b * c - d * a));
             long y = Math.round((e / c) - (a * x / c));
-            if (x < 0 || y < 0) {
+            if (x < 0 || y < 0 || (buttonPressLimit != null && (x > buttonPressLimit || y > buttonPressLimit))) {
                 continue;
             }
             if (a * x + c * y == e && b * x + d * y == f) {
                 totalTokens += 3 * x + y;
             }
         }
+        return totalTokens;
+    }
+
+    public static void solvePartTwo() {
+        for (int i = 0; i < machines.length; i++) {
+            machines[i][4] += 10000000000000L;
+            machines[i][5] += 10000000000000L;
+        }
+        long totalTokens = solve(null);
         System.out.println("SOLUTION DAY 13 PART 2: " + totalTokens);
     }
 
